93775a9ce8e780092f0910c385dfb5b79c8eb9d9
chenhuan <chenhuan@qfang.com>
2020-11-17 11:22:57 +0800

    conflict

--- Week_03/src/main/java/com/weekwork/io/gateaway/outbound/netty/NettyHttpClient.java
+++ Week_03/src/main/java/com/weekwork/io/gateaway/outbound/netty/NettyHttpClient.java
@@ -2,17 +2,18 @@ package com.weekwork.io.gateaway.outbound.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.expression.spel.ast.NullLiteral;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
@@ -21,7 +22,7 @@ public class NettyHttpClient {
 
     private String host;
 
-  //  private Channel clientChannel;
+    //  private Channel clientChannel;
 
 
     private EventLoopGroup group = new NioEventLoopGroup();
