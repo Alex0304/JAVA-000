88c4d6d65773b52649e0b1418f8bb2a3897e6843
chenhuan <chenhuan@qfang.com>
2021-02-23 19:48:06 +0800

    git test version finished

--- Week_12/src/main/java/com/weekwork/WeekWork12Application.java
+++ Week_12/src/main/java/com/weekwork/WeekWork12Application.java
@@ -10,5 +10,6 @@ public class WeekWork12Application {
     public static void main(String[] args) {
         SpringApplication.run(WeekWork12Application.class);
         System.out.println("git branch test v1.1");
+        System.out.println("git branch test v1.0 finished");
     }
 }
